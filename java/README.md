a05f556b18d2cad4d18ac5a2af69a88a9dd80622
jancajthaml <jan.cajthaml@gmail.com>
2013-11-14 20:47:41 +0100

    cleanup

--- java/README.md
+++
@@ -1,4 +0,0 @@
-datastructures
-==============
-
-data structures
--- java/SquareMatrix.java
+++ java/SquareMatrix.java
@@ -21,98 +21,88 @@ public class SquareMatrix<T>
 
    public int determinant( int n )
    {
-	   double[][] LU = new double[n][n];
-	   
+       double[][] LU = new double[n][n];
+
        for( int i=0; i<n; i++ )
        for( int j=0; j<n; j++ )
     	   LU[i][j] = this.data[i][j];
-	      
-	   int[] piv = new int[n];
-	   
-	   for( int i=0; i<n; i++ )
-		   piv[i] = i;
-	      
-	   int sign			= 1;
-	   double[] LUrowi	= null;
-	   double[] LUcolj	= new double[n];
 
-	   for( int j=0; j<n; j++ )
-	   {
-		   for( int i=0; i<n; i++ )
-			   LUcolj[i] = LU[i][j];
+       int[] piv = new int[n];
 
-		   for( int i=0; i<n; i++ )
-		   {
-			   LUrowi = LU[i];
+       for( int i=0; i<n; i++ ) piv[i] = i;
 
-			   int kmax	= Math.min(i,j);
-			   double s	= 0.0;
+       int       sign   = 1             ;
+       double[]  LUrowi	= null          ;
+       double[]  LUcolj	= new double[n] ;
 
-			   for( int k=0; k<kmax; k++ )
-				   s += LUrowi[k]*LUcolj[k];
-	    		  
-			   LUrowi[j] = LUcolj[i] -= s;
-		   }
-	   
-		   int p = j;
-	         
-		   for( int i=j+1; i<n; i++ ) if( Math.abs(LUcolj[i])>Math.abs(LUcolj[p]) )
-			   p = i;
-		   
-		   if( p!=j )
-		   {
-			   for( int k=0; k<n; k++ )
-			   {
-				   double t = LU[p][k];
-				   LU[p][k] = LU[j][k];
-				   LU[j][k] = t;
-			   }
-	        	 
-			   int k	= piv[p];
-			   piv[p]	= piv[j];
-			   piv[j]	= k;
-			   sign		= -sign;
-		   }
+       for( int j=0; j<n; j++ )
+       {
+           for( int i=0; i<n; i++ )  LUcolj[i] = LU[i][j];
+           for( int i=0; i<n; i++ )
+           {
+               LUrowi   = LU[i]         ;
+               int kmax = Math.min(i,j) ;
+               double s	= 0.0           ;
 
-		   if( j<n & LU[j][j] != 0.0 ) for( int i=j+1; i<n; i++ )
-			   LU[i][j] /= LU[j][j];
-	   }
+               for( int k=0; k<kmax; k++ )  s += LUrowi[k]*LUcolj[k];
 
-	   int d = sign;
-		   
-	   for( int j=0; j<n; j++ )
-		   d *= LU[j][j];
+               LUrowi[j] = LUcolj[i] -= s;
+           }
 
-	   return d;
+           int p = j;
+
+           for( int i=j+1; i<n; i++ ) if( Math.abs(LUcolj[i])>Math.abs(LUcolj[p]) )  p = i;
+           if( p!=j )
+           {
+               for( int k=0; k<n; k++ )
+               {
+                   double t = LU[p][k] ;
+                   LU[p][k] = LU[j][k] ;
+                   LU[j][k] = t        ;
+               }
+
+               int k  = piv[p] ;
+               piv[p] = piv[j] ;
+               piv[j] = k      ;
+               sign   = -sign  ;
+           }
+
+           if( j<n & LU[j][j] != 0.0 ) for( int i=j+1; i<n; i++ )  LU[i][j] /= LU[j][j];
+       }
+
+       int d = sign;
+
+       for( int j=0; j<n; j++ )  d *= LU[j][j];
+
+       return d;
    }
 
    public void print()
    {
-	   System.out.println();
+       System.out.println();
 	   
-	   //SPEED HACK
-	   int i=-1;
-	   int j=-1;
+       //SPEED HACK
+       int i = -1 ;
+       int j = -1 ;
 	   
-	   while( ++i<n+2 )
-	   {
-		   j=-1;
-		   while( ++j<n+2 )
-			   System.out.print(" "+((int)(data[i][j])));
-		   System.out.println();
-	   }
-	   System.out.println();
+       while( ++i<n )
+       {
+           j=-1;
+           while( ++j<n ) System.out.print(" "+((int)(data[i][j])));
+           System.out.println();
+       }
+       System.out.println();
    }
 
    public int[][] toArray()
    {
-	   int[][] result = new int[n][n];
+       int[][] result = new int[n][n];
 
        for( int i=0; i<data.length; i++ )
        for( int j=0; j<data.length; j++ )
-    	   result[i][j] = (int)data[i][j];
+           result[i][j] = (int)data[i][j];
 
-	   return result;
-   }
+       return result;
+    }
 
 }
